8c51a120648e253b96087b04517d51fe48ef764d
taiit <vohuutai27@gmail.com>
2024-03-18 23:17:47 +0700

    Update GPICase default config from Recalbox

--- 00_install_sw.sh
+++ 00_install_sw.sh
@@ -11,3 +11,13 @@ echo "Install linux kernel build toolchain"
 
 $APT_GET git bc bison flex libssl-dev make libncurses-dev
 
+# Install GUI
+#    XOG. sudo apt install xserver-xorg -y
+#    XFCE: sudo apt install xfce4 xfce4-terminal -y
+#    Display Manage:  sudo apt install lightdm -y
+#       Select the display server: sudo systemctl get-default && sudo systemctl set-default graphical.target
+#       Select the display manager: sudo dpkg-reconfigure lightdm
+#       Select the session manager: sudo update-alternatives --config x-session-manager
+#       Select the window manager: sudo update-alternatives --config x-window-manager
+
+#  --> sudo reboot
